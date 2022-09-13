53cbd0bfaed6cda58caa5718eccd880e49e14582
Michael Rommel <michamaxe@gmail.com>
2022-09-13 20:00:51 +0200

    add more tests

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,22 +1,52 @@
 import unittest
 
 from simulation.configuration import SimulationConfiguration, SimulationConfigurationGermany2019
-from simulation.data import SimulationDataGermany2019
+from simulation.data import SimulationDataGermany2019, SimulationData
 from simulation.simulation import Simulation
 
 
 class TestSimulation(unittest.TestCase):
 	
+	def test_set_males(self):
+		# GIVEN
+		config = SimulationConfigurationGermany2019()
+		data = SimulationData()
+		sim = Simulation(config=config, data=data)
+		population_before = sim.population
+		
+		# WHEN
+		sim.data.set_number_of_males(25, 2000)
+		
+		# THEN
+		population_after = sim.population
+		self.assertEqual(population_before, 0)
+		self.assertEqual(population_after, 2000)
+	
+	def test_set_females(self):
+		# GIVEN
+		config = SimulationConfigurationGermany2019()
+		data = SimulationData()
+		sim = Simulation(config=config, data=data)
+		population_before = sim.population
+		
+		# WHEN
+		sim.data.set_number_of_females(25, 3000)
+		
+		# THEN
+		population_after = sim.population
+		self.assertEqual(population_before, 0)
+		self.assertEqual(population_after, 3000)
+	
 	def test_basic_population(self):
 		# GIVEN
 		config = SimulationConfigurationGermany2019()
 		data = SimulationDataGermany2019()
-		sim = Simulation(config = config, data = data)
+		sim = Simulation(config=config, data=data)
 		population_before = sim.population
 		
 		# WHEN
 		pass
-
+		
 		# THEN
 		population_after = sim.population
 		self.assertEqual(population_after, population_before)
@@ -25,12 +55,12 @@ class TestSimulation(unittest.TestCase):
 		# GIVEN
 		config = SimulationConfiguration(81.8, 3.5)  # number_of_children_per_women must bigger than usual
 		data = SimulationDataGermany2019()
-		sim = Simulation(config = config, data = data)
+		sim = Simulation(config=config, data=data)
 		population_before = sim.population
 		
 		# WHEN
 		sim.iterate()
-
+		
 		# THEN
 		population_after = sim.population
 		self.assertGreater(population_after, population_before)
