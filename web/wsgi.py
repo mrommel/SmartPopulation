8e6f11c00b3351756952fe4d033f5b2a814300e9
Michael Rommel <michamaxe@gmail.com>
2022-09-20 22:26:58 +0200

    refine template

--- web/wsgi.py
+++ web/wsgi.py
@@ -5,5 +5,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def index():
+	return render_template('index.html')
+
+
+@app.route("/dashboard")
+def dashboard():
+	return render_template('index.html')
+
+
+@app.route("/simulations")
+def simulations():
+	return render_template('simulations.html')
+
+@app.route("/groups")
+def groups():
+	return render_template('groups.html')
+
+
+@app.route("/situations")
+def situations():
+	return render_template('situations.html')
